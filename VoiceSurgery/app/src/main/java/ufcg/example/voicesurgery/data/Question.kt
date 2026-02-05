2817a27b7e510fadf3b0b5a5fb18fc47fc1aa842
José Edson Carneiro Agra Júnior <102812154+junior25dunedain@users.noreply.github.com>
2026-02-05 09:26:12 -0300

    Delete VoiceSurgery directory

--- VoiceSurgery/app/src/main/java/ufcg/example/voicesurgery/data/Question.kt
+++
@@ -1,8 +0,0 @@
-package ufcg.example.voicesurgery.data
-
-sealed class Question(val title: String)
-
-class MultipleChoiceQuestion(title: String, val options: List<String>) : Question(title)
-class TextInputQuestion(title: String) : Question(title)
-class CheckboxQuestion(title: String, val options: List<String>) : Question(title)
-class SalvaTempo(title: String) : Question(title)
--- VoiceSurgery/app/src/main/java/ufcg/example/voicesurgery/network/LoginResponse.kt
+++
@@ -1,6 +0,0 @@
-package ufcg.example.voicesurgery.network
-
-// Certifique-se de que o nome da variável bate com o JSON de resposta
-data class LoginResponse(
-    val access_token: String
-)
